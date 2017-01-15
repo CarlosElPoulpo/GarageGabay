1b099f7a249db578f63925837880864e0291966f
Nathan De Pachtere <de.pachtere.nat@gmail.com>
2017-01-15 14:27:30 +0100

    Initial design commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,15 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/apropos", name="apropos")
+     */
+    public function aproposAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/apropos.html.twig');
     }
 }
