ccf775c1d87845ab83404af05c24d4e5b6ed1928
Nathan De Pachtere <de.pachtere.nat@gmail.com>
2017-03-22 14:56:55 +0100

    Form needs to be in Form/Type folder

--- src/GarageBundle/Form/Type/ContactMailNCType.php
+++ src/GarageBundle/Form/Type/ContactMailNCType.php
@@ -1,31 +1,31 @@
 <?php
-namespace GarageBundle\Form;
+namespace GarageBundle\Form\Type;
 
 use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
-use GarageBundle\Entity\ContactMail;
+use GarageBundle\Entity\ContactMailNC;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ContactMailType extends AbstractType
+class ContactMailNCType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('fullname', null, array('label'=>"Prénom et nom"))
         ->add('email', EmailType::class, array('label'=>"Adresse email"))
         ->add('phone', null, array('label'=>"Téléphone"))
-        ->add('body', TextareaType::class, array('label'=>"Texte", "attr"=>array('class'=> "md-textarea")))
+        ->add('essai', CheckboxType::class, array('label'=>"Demander un essai", 'required' => false))
         ->add('recaptcha', EWZRecaptchaType::class, array('label'=>" "))
-        ->add('save', SubmitType::class, array('label'=>"Envoyer un mail", "attr"=>array('class'=> "btn btn-large btn-renault-tertiary-grey btn-cent-percent-width")));
+        ->add('save', SubmitType::class, array('label'=>"Envoyer", "attr"=>array('class'=> "btn btn-large btn-renault-tertiary-grey btn-cent-percent-width")));
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => ContactMail::class,
+            'data_class' => ContactMailNC::class,
         ));
     }
 }
